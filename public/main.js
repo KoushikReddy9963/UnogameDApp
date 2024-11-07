2b7e82cc31ca9418ecfc0b801dceca78597df31c
KoushikReddy9963 <yennamkoushikreddy1234@gmail.com>
2024-11-07 14:55:08 +0530

    Change in main.js

--- public/main.js
+++ public/main.js
@@ -12,7 +12,19 @@ let hand = [];
 let turn;
 let playerName;
 
-function init() {
+async function init() {
+  if (typeof window.ethereum !== 'undefined') {
+    const web3 = new Web3(window.ethereum);
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    const accounts = await web3.eth.getAccounts();
+    const balance = await web3.eth.getBalance(accounts[0]);
+    const formattedBalance = web3.utils.fromWei(balance, 'ether');
+    alert(`Address: ${accounts[0]}\nBalance: ${formattedBalance} ETH`);
+  } else {
+    alert("Please install MetaMask to play the game.");
+    return;
+  }
+
   ctx.font = "12px Arial";
   canvas.style.backgroundColor = '#10ac84';
   cards.src = 'images/deck.svg';
