b3a551c1be6912f3720476304aa089f18549a848
KoushikReddy9963 <yennamkoushikreddy1234@gmail.com>
2024-11-13 21:33:13 +0530

    Configuration-added

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/be8d335d6a7f47df9d23572a28233647",    // Add your Sepolia RPC URL here
-      accounts: ['b9433accc50d9f7fd37f0c43540555b86f9f8b3cea1dcff836933c64874a1d1c']  // Add your private key here
+      url: "https://sepolia.infura.io/v3/be8d335d6a7f47df9d23572a28233647", 
+      accounts: ['b9433accc50d9f7fd37f0c43540555b86f9f8b3cea1dcff836933c64874a1d1c'] 
   }
   }
 };
